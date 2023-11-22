206fabc79b47a370c8a22f5adb296bc63b61246d
Eugenio <eugenio.spera@gmail.com>
2023-11-22 17:21:21 -0300

    Acrescentado novas formulas ao arquivo

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 let num1 = prompt ("Digite um número:");
 
 let num2 = prompt ("Digite outro número:");
@@ -21,5 +22,60 @@ if (op == "+"){
 
 alert ('O resultado da op é'+soma);
     
-    
+
+
+let num1 = prompt ("Qual a KM percorrida:");
+
+let num2 = prompt ("Quantos dias foi alugado:");
+
+let soma;
+
+soma = (Number(num1)*0.20) + (Number(num2)*90);
+
+alert ('O resultado da op é' +soma);
+
+
+let dist = prompt ("Qual a KM que deseja percorrer:");
+
+let total;
+
+if (dist <=200){
+    total = Number(dist)*0.50;    
+}else{
+    total = Number(dist)*0.45;
+}
+
+  
+
+alert ('Total a pagar'+total);
+
+ 
+
+
+let n1 = prompt ("Nota 1:");
+
+let n2 = prompt ("Nota 2:");
+
+media = (Number(n1) + Number(n2)) /2;
+
+if (media < 4.9){
+    alert("Reprovado");    
+}else if
+    (media >=5 && conv<6.9){
+    alert("Recuperação");    
+}else{ 
+    alert("Aprovado");
+}     
+  */ 
+
+
+  
+
+let n1 prompt ("Nome do funcionario:");
+
+let n2 prompt ("Salario do funcionario:");
+
+let n3 prompt ("Anos de empresa do funcionario:");
+
+Reaj = (Number(n2)
 
