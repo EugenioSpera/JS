ef1ecf3e7218c89dd71f4d9859830bdc9c322734
Eugenio <eugenio.spera@gmail.com>
2023-11-29 17:28:49 -0300

    Add mais exercícios Tabuada

--- script.js
+++ script.js
@@ -121,7 +121,7 @@ else{
  
     alert( " Valor a receber é: R$" + val );
 
-*/
+
 
 let gen = prompt("Digite o seu gênero:");
 let sal = prompt("Digite o seu salário:");
@@ -156,3 +156,54 @@ salf = Number(conv) + Number(sal);
  
  
 alert( " Seu novo salário é: R$" + salf );
+
+
+let pes = prompt("Digite o seu peso:");
+let alt = prompt("Digite a sua altura:");
+
+val = Number(pes) / (Number(alt) * Number(alt));
+
+if(val <= 18.5){
+    alert("Abaixo do peso");   
+}
+
+else if (val > 18.25 && val <= 25){
+    alert("Peso ideal");    
+}
+else if (val > 25 && val <= 30){
+        alert("Sobrepeso");  
+}
+else if (val > 30 && val <= 40){
+            alert("Obesidade"); 
+}            
+
+else { 
+    alert("Obesidade mórbida");
+}     
+*/
+
+function mostrarTabuada() {
+//cria referencia aos elemntos da pagia
+let inNumero = document.getElementById('inNumero');
+let outTabuada = document.getElementById('outTabuada');
+
+//converte conteudo do campo inNUmero
+let numero = Number(inNumero.value);
+    
+//cria uma variável do tipo String, que irá concatenar a resposta
+let resposta = "";
+
+//cria um laço de repetição
+for (let i = 1; i <= 10; i++) {
+// a variável resposta vai acumulando os novos conteúdos
+resposta = resposta + numero + " x " + i + " = " + numero * i + "\n"
+}
+
+// O conteúdo da tag pre é alterado para exibir a tabuada do numero
+
+outTabuada.textContent = resposta;
+}
+//cria referência ao botão e após associa function ao evento click 
+
+let btMostrar = document.getElementById('btMostrar');
+btMostrar.addEventListener("click", mostrarTabuada);
